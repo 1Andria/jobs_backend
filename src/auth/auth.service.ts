40a90836893083d879732c14e14ca957d2ec2db9
1Andria <andriamelua22@gmail.com>
2025-08-09 15:44:12 +0400

    core

--- src/auth/auth.service.ts
+++ src/auth/auth.service.ts
@@ -46,7 +46,6 @@ export class AuthService {
     );
 
     const inviteLink = `${process.env.FRONT_URL}/auth/verify-company-account?token=${token}`;
-
     await this.emailSenderService.sendInviteLink(email, inviteLink);
     return {
       message:
